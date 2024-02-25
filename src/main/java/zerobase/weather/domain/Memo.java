ec2c8c74c358444a01d63bf541129e9199bbf11d
DemianStudio <rudejrqkr97@gmail.com>
2024-02-26 00:32:05 +0900

    :white_check_mark: [Add] JPA 방식으로 데이터 저장 및 조회 + 테스트 코드 통과

--- src/main/java/zerobase/weather/domain/Memo.java
+++ src/main/java/zerobase/weather/domain/Memo.java
@@ -1,13 +1,21 @@
 package zerobase.weather.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.context.annotation.Primary;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
+@Entity(name = "Memo")
 public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String text;
 }
